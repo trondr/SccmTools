f047eae3d77867e994a95221e6fb18eb4490ba21
trondr <trondr@outlook.com>
2015-12-05 18:56:36 +0100

    SccmTools.1.0.15339.2

--- src/SccmTools.Library/Properties/AssemblyInfo.cs
+++ src/SccmTools.Library/Properties/AssemblyInfo.cs
@@ -15,12 +15,12 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("SccmTools provides various commands for Sccm 2012 interaction")]
-[assembly: AssemblyCompany("github.com/trondr")]
+[assembly: AssemblyCompany("github.com.trondr")]
 [assembly: AssemblyProduct("SccmTools.Library")]
 [assembly: AssemblyCopyright("Copyright © github.com.trondr 2015")]
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyInformationalVersion("1.0.15319.1.608a76f")]
-[assembly: AssemblyVersion("1.0.15319.1")]
-[assembly: AssemblyFileVersion("1.0.15319.1")]
+[assembly: AssemblyInformationalVersion("1.0.15339.2.c45fc02")]
+[assembly: AssemblyVersion("1.0.15339.2")]
+[assembly: AssemblyFileVersion("1.0.15339.2")]
 
 
